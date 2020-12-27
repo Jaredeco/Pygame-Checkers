91a33a136675c45860a304b5fc64bb0f1c507233
Jaredeco <jano.redecha@gmail.com>
2020-12-27 13:09:15 +0100

    Added first files

---
+++ checkers.py
@@ -0,0 +1,168 @@
+import pygame
+from pygame import gfxdraw
+
+pygame.init()
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+FPS = 60
+CLOCK = pygame.time.Clock()
+win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Checkers")
+icon = pygame.image.load('assets/icon.png')
+pygame.display.set_icon(icon)
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (238, 59, 59)
+green = (127, 255, 0)
+blue = (0, 255, 255)
+
+
+class Piece:
+    def __init__(self, x, y, i, j, color):
+        self.x = x
+        self.y = y
+        self.i = i
+        self.j = j
+        self.color = color
+        self.radius = (SCREEN_WIDTH / 8) / 3
+
+    def draw(self):
+        gfxdraw.filled_circle(win, int(self.x), int(self.y), int(self.radius), self.color)
+
+    @staticmethod
+    def _remove_highlight():
+        for r, c in BOARD.av_pos:
+            BOARD.game[r][c].highlight()
+        BOARD.av_pos = None
+
+    @staticmethod
+    def coor_from_pos(r, c):
+        dim = 8
+        x = BOARD.game[r][c].x + (SCREEN_WIDTH / dim) / 2
+        y = BOARD.game[r][c].y + (SCREEN_WIDTH / dim) / 2
+        return x, y
+
+    def move(self, i, j):
+        if (i, j) in BOARD.av_pos and (i, j) != BOARD.av_pos[0]:
+            BOARD.game[self.i][self.j].piece = None
+            x, y = self.coor_from_pos(i, j)
+            self.x, self.y, self.i, self.j = x, y, i, j
+            BOARD.game[i][j].piece = self
+            BOARD.turn = blue if BOARD.turn == red else red
+            self._remove_highlight()
+        else:
+            self._remove_highlight()
+
+
+class Position:
+    def __init__(self, x, y, i, j, color, piece):
+        self.x = x
+        self.y = y
+        self.i = i
+        self.j = j
+        self.color = color
+        self.piece = piece
+        self.size = SCREEN_WIDTH / 8
+        self.highlighted = False
+
+    def draw(self):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.size, self.size))
+        if self.highlighted:
+            pygame.draw.rect(win, green, (self.x + 1.5, self.y + 1.5, self.size - 1.5, self.size - 1.5), 3)
+        if self.piece is not None:
+            self.piece.draw()
+
+    def highlight(self):
+        if not self.highlighted:
+            self.highlighted = True
+        else:
+            self.highlighted = False
+
+
+class Board:
+    def __init__(self):
+        self.turn = blue
+        self.av_pos = None
+        dim = 8
+        self.size = SCREEN_WIDTH // dim
+        self.game = [[] for i in range(dim)]
+        for i in range(dim):
+            for j in range(dim):
+                pos = Position(self.size * j + 2, self.size * i + 2, i, j,
+                               white if (not j % 2 and not i % 2) or (j % 2 and i % 2) else black, None)
+                if (not i % 2 and j % 2 and (5 <= i or i <= 2)) or (i % 2 and not j % 2 and (5 <= i or i <= 2)):
+                    pos.piece = Piece(pos.x + (SCREEN_WIDTH / dim) / 2, pos.y + (SCREEN_WIDTH / dim) / 2, i, j,
+                                      blue if i <= 2 else red)
+                self.game[i].append(pos)
+
+    def draw(self):
+        for i in range(len(self.game)):
+            for j in range(len(self.game[0])):
+                self.game[i][j].draw()
+
+    def get_row_col_clicked(self, pos):
+        row = pos[1] // self.size
+        col = pos[0] // self.size
+        return row, col
+
+    @staticmethod
+    def is_valid(i, j):
+        if 0 <= i <= 7 and 0 <= j <= 7:
+            return True
+        return False
+
+    def available_pos(self, i, j, idx=None):
+        av_pos = [(i + 1, j + 1), (i + 1, j - 1), (i - 1, j + 1), (i - 1, j - 1)]
+        av_pos = av_pos[:2] if self.turn == blue else av_pos[2:]
+        if idx is not None:
+            return av_pos[idx]
+        pos = [(i, j)]
+        for i, (r, c) in enumerate(av_pos):
+            if self.is_valid(r, c):
+                if self.game[r][c].piece is None:
+                    pos.append((r, c))
+                else:
+                    rs, cs = self.available_pos(r, c, i)
+                    if self.is_valid(rs, cs) and self.game[rs][cs].piece is None:
+                        pos.append((rs, cs))
+        return pos
+
+    def select_move(self, r, c):
+        if self.av_pos is None:
+            piece = self.game[r][c].piece
+            if piece is not None and piece.color == self.turn:
+                av_pos = self.available_pos(r, c)
+                self.av_pos = av_pos
+                for i, j in av_pos:
+                    self.game[i][j].highlight()
+        else:
+            i, j = self.av_pos[0][0], self.av_pos[0][1]
+            piece = self.game[i][j].piece
+            piece.move(r, c)
+
+    def winner(self):
+        pass
+
+    def reset(self):
+        self.__init__()
+
+
+def redraw_win():
+    win.fill(blue if BOARD.turn == blue else red)
+    BOARD.draw()
+    pygame.display.flip()
+
+
+BOARD = Board()
+RUN = True
+while RUN:
+    CLOCK.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            RUN = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            r, c = BOARD.get_row_col_clicked(event.pos)
+            BOARD.select_move(r, c)
+    redraw_win()
+pygame.quit()
